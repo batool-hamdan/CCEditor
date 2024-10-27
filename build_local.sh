81a886440f38afac5400c9e6ed5d3946ae31e671
batool-hamdan <raedbatool590@gmail.com>
2024-10-28 01:18:10 +0300

    chore: deploy using new aws runner

---
+++ build_local.sh
@@ -0,0 +1,7 @@
+cd backend && mvn clean install -DskipTests
+cd .. && docker build -t batool83/backend:latest -f backend/Dockerfile backend && docker build -t batool83/frontend:latest -f frontend/Dockerfile frontend
+docker login -u batool83 -p BeBo@1998
+docker push batool83/backend:latest && docker push batool83/frontend:latest
+docker-compose -f docker-compose.yml down
+sleep 10;
+docker-compose -f docker-compose.yml up --build -d
